6dafeefba9ebfc6f8bc6f78398552eb3fc14175d
wenincode <tyler.wendlandt@hashicorp.com>
2022-11-16 21:32:06 -0700

    Fix lint errors

--- my-fake-addon/src/components/c-u-t/fake-button.js
+++ my-fake-addon/src/components/c-u-t/fake-button.js
@@ -4,6 +4,6 @@ import { action } from '@ember/object';
 export default class FakeButton extends Component {
   @action
   handleClick() {
-    console.log('You clicked this button, didn\'t you?');
+    console.log("You clicked this button, didn't you?");
   }
 }
